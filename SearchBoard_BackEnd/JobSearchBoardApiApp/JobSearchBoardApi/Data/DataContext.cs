acb29084130ebc9170577088d14575d6021b6963
Andrey Ivanov <68381704+andreyIvnov@users.noreply.github.com>
2022-05-26 16:33:08 +0300

    DbContext creatin by nuget instalatio

---
+++ SearchBoard_BackEnd/JobSearchBoardApiApp/JobSearchBoardApi/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using JobSearchBoardApi.Models;
+using Microsoft.EntityFrameworkCore;    //Instaled by Microsoft.EntityFrameworkCore nuget
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSearchBoardApi.Data
+{
+    public class DataContext    : DbContext   
+    {
+        public DataContext(DbContextOptions<DataContext> opts) : base(opts) { }
+
+        public DbSet<Company> Companies{ get; set; }
+        public DbSet<Employee> Employees { get; set; }  
+        public DbSet<IndastryType> IndastryTypes { get; set;}
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<JobStatus> JobStatuses { get; set; }   
+    }
+}
